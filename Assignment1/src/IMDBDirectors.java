437a6d104f4c419bfa6bfc30366d922841d955f0
Danie <Danie@169.254.129.4>
2017-02-03 13:04:06 -0500

    Skeleton

--- Assignment1/src/IMDBDirectors.java
+++ Assignment1/src/IMDBDirectors.java
@@ -1,4 +1,36 @@
+//import java.awt.List;
+import java.nio.file.Path;
+import java.util.List;
+
 
 public class IMDBDirectors {
+	
+	public static long countMovies(Path file){
+		
+	}
+	public static long countDirectors(Path file){
+		
+	}
+	public static List<String> getMoviesOfDirector(Path file, String last, String first, SortBy sort){
+		
+	}
+	public static List<String> getMoviesInYear(Path file, int year, SortBy sort){
+		
+	}
+	public static List<String> getDirectorWithMostMovies(Path file){
+		
+	}
+	public static List<String> getDirectorsOfMovie(Path file, String movie, int year, boolean sortByLastname){
+		
+	}
+	public static List<String> getNumberOfDirectorsPerMovie(Path file, boolean sortByMovie){
+		
+	}
+	public static List<String> getNumberOfMoviesPerYear(Path file, boolean sortByYear){
+		
+	}public static List<String> getDirectorsByNumberOfMovies(Path file, boolean sortByName){
+		
+	}
 
+	
 }
